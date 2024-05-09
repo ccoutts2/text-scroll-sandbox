4884253360d34253d9845b6fd90848df895f0b06
Chris Coutts <chris.dcoutts@gmail.com>
2024-05-09 11:01:38 +0100

    First commit

---
+++ src/app/page.jsx
@@ -0,0 +1,158 @@
+export default function Home() {
+  return (
+    <main>
+      <div class="container">
+        <div class="sticky-bar">
+          <div class="item">
+            <p>Barrett</p>
+          </div>
+          <div class="item">
+            <p>&</p>
+          </div>
+          <div class="item">
+            <p>Hale</p>
+          </div>
+        </div>
+
+        <section class="hero">
+          <img src="./assets/hero.jpg" alt="" />
+        </section>
+
+        <section class="clients">
+          <div class="row">
+            <div class="logo">
+              <p>Logo 1</p>
+            </div>
+            <div class="logo">
+              <p>Logo 2</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="logo">
+              <p>Logo 3</p>
+            </div>
+            <div class="logo">
+              <p>Logo 4</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="logo">
+              <p>Logo 5</p>
+            </div>
+            <div class="logo">
+              <p>Logo 6</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="logo">
+              <p>Logo 7</p>
+            </div>
+            <div class="logo">
+              <p>Logo 8</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="logo">
+              <p>Logo 9</p>
+            </div>
+            <div class="logo">
+              <p>Logo 10</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="logo">
+              <p>Logo 11</p>
+            </div>
+            <div class="logo">
+              <p>Logo 12</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="logo">
+              <p>Logo 13</p>
+            </div>
+            <div class="logo">
+              <p>Logo 14</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="logo">
+              <p>Logo 15</p>
+            </div>
+            <div class="logo">
+              <p>Logo 16</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="logo">
+              <p>Logo 17</p>
+            </div>
+            <div class="logo">
+              <p>Logo 18</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="logo">
+              <p>Logo 19</p>
+            </div>
+            <div class="logo">
+              <p>Logo 20</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="logo">
+              <p>Logo 21</p>
+            </div>
+            <div class="logo">
+              <p>Logo 22</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="logo">
+              <p>Logo 23</p>
+            </div>
+            <div class="logo">
+              <p>Logo 24</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="logo">
+              <p>Logo 25</p>
+            </div>
+            <div class="logo">
+              <p>Logo 26</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="logo">
+              <p>Logo 27</p>
+            </div>
+            <div class="logo">
+              <p>Logo 28</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="logo">
+              <p>Logo 29</p>
+            </div>
+            <div class="logo">
+              <p>Logo 30</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="logo">
+              <p>Logo 31</p>
+            </div>
+            <div class="logo">
+              <p>Logo 32</p>
+            </div>
+          </div>
+        </section>
+
+        <section class="trigger-footer">
+          <img src="./assets/footer.jpg" alt="" />
+        </section>
+      </div>
+    </main>
+  );
+}
