cfe6f8829a14ec014be947206a6d97e53525d427
Chris Coutts <chris.dcoutts@gmail.com>
2024-05-09 18:20:37 +0100

    Add scroll trigger animation

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,158 +1,209 @@
+"use client";
+import React, { useEffect, useLayoutEffect, useRef } from "react";
+import Lenis from "lenis";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+const clients = [
+  {
+    client: "wedd1",
+    logo: "logo 1",
+  },
+  {
+    client: "wedd1",
+    logo: "logo 1",
+  },
+  {
+    client: "wedd1",
+    logo: "logo 1",
+  },
+  {
+    client: "wedd1",
+    logo: "logo 1",
+  },
+  {
+    client: "wedd1",
+    logo: "logo 1",
+  },
+  {
+    client: "wedd1",
+    logo: "logo 1",
+  },
+  {
+    client: "wedd1",
+    logo: "logo 1",
+  },
+  {
+    client: "wedd1",
+    logo: "logo 1",
+  },
+  {
+    client: "wedd1",
+    logo: "logo 1",
+  },
+  {
+    client: "wedd1",
+    logo: "logo 1",
+  },
+  {
+    client: "wedd1",
+    logo: "logo 1",
+  },
+  {
+    client: "wedd1",
+    logo: "logo 1",
+  },
+  {
+    client: "wedd1",
+    logo: "logo 1",
+  },
+  {
+    client: "wedd1",
+    logo: "logo 1",
+  },
+  {
+    client: "wedd1",
+    logo: "logo 1",
+  },
+  {
+    client: "wedd1",
+    logo: "logo 1",
+  },
+];
+
 export default function Home() {
+  const container = useRef(null);
+  const stickyBar = useRef(null);
+  const footerTrigger = useRef(null);
+  const rows = useRef([]);
+  const footerTriggerHeight = footerTrigger.current?.offsetHeight || 0;
+
+  function useScrollTriggerAnimation(refs) {
+    useGSAP(
+      () => {
+        gsap.registerPlugin(ScrollTrigger);
+
+        function getStickyBarCenter() {
+          const stickyBar = document.querySelector(".sticky-bar");
+          return stickyBar.offsetTop + stickyBar.offsetHeight / 2;
+        }
+
+        refs.current.forEach((row, index) => {
+          ScrollTrigger.create({
+            trigger: row,
+            start: () => `top+=${getStickyBarCenter() - 450} center`,
+            end: () => `top+=${getStickyBarCenter() - 350} center`,
+            scrub: true,
+            onUpdate: (self) => {
+              const progress = self.progress;
+              const maxGap = window.innerWidth < 900 ? 10 : 15;
+              const minGap = window.innerWidth < 900 ? 0.5 : 1;
+              const currentGap = minGap + (maxGap - minGap) * progress;
+              row.style.gap = `${currentGap}em`;
+            },
+          });
+
+          ScrollTrigger.create({
+            trigger: row,
+            start: () => `top+=${getStickyBarCenter() - 400} center`,
+            end: () => `top+=${getStickyBarCenter() - 300} center`,
+            scrub: true,
+            onUpdate: (self) => {
+              const progress = self.progress;
+              const maxGap = window.innerWidth < 900 ? 0.5 : 1;
+              const minGap = window.innerWidth < 900 ? 10 : 15;
+              const currentGap = minGap + (maxGap - minGap) * progress;
+              row.style.gap = `${currentGap}em`;
+            },
+          });
+        });
+
+        ScrollTrigger.create({
+          trigger: footerTrigger.current,
+          start: "top bottom",
+          end: "bottom bottom",
+          scrub: true,
+          onUpdate: (self) => {
+            const startTop = 50;
+            const endTop = 92;
+            const newTop = startTop + (endTop - startTop) * self.progress;
+            stickyBar.current.style.top = `${newTop}%`;
+          },
+        });
+
+        ScrollTrigger.create({
+          trigger: footerTrigger.current,
+          start: () =>
+            `top+=${footerTriggerHeight - (window.innerHeight + 100)} bottom`,
+          end: "bottom bottom",
+          scrub: true,
+          onUpdate: (self) => {
+            const fontSizeStart = window.innerWidth < 900 ? 2.5 : 1.25;
+            const fontSizeEnd = 9;
+            const newFontSize =
+              fontSizeStart + (fontSizeEnd - fontSizeStart) * self.progress;
+            stickyBar.current.querySelectorAll("p").forEach((p) => {
+              p.style.fontSize = `${newFontSize}vw`;
+            });
+          },
+        });
+      },
+      { scope: container }
+    );
+  }
+
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    lenis.on("scroll", (e) => {
+      console.log(e);
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
+  useScrollTriggerAnimation(rows);
+
   return (
-    <main>
-      <div class="container">
-        <div class="sticky-bar">
-          <div class="item">
-            <p>Barrett</p>
-          </div>
-          <div class="item">
-            <p>&</p>
-          </div>
-          <div class="item">
-            <p>Hale</p>
-          </div>
+    <div ref={container} className="container">
+      <div ref={stickyBar} className="sticky-bar">
+        <div className="item">
+          <p>e</p>
         </div>
-
-        <section class="hero">
-          <img src="./assets/hero.jpg" alt="" />
-        </section>
-
-        <section class="clients">
-          <div class="row">
-            <div class="logo">
-              <p>Logo 1</p>
-            </div>
-            <div class="logo">
-              <p>Logo 2</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="logo">
-              <p>Logo 3</p>
-            </div>
-            <div class="logo">
-              <p>Logo 4</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="logo">
-              <p>Logo 5</p>
-            </div>
-            <div class="logo">
-              <p>Logo 6</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="logo">
-              <p>Logo 7</p>
-            </div>
-            <div class="logo">
-              <p>Logo 8</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="logo">
-              <p>Logo 9</p>
-            </div>
-            <div class="logo">
-              <p>Logo 10</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="logo">
-              <p>Logo 11</p>
-            </div>
-            <div class="logo">
-              <p>Logo 12</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="logo">
-              <p>Logo 13</p>
-            </div>
-            <div class="logo">
-              <p>Logo 14</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="logo">
-              <p>Logo 15</p>
-            </div>
-            <div class="logo">
-              <p>Logo 16</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="logo">
-              <p>Logo 17</p>
-            </div>
-            <div class="logo">
-              <p>Logo 18</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="logo">
-              <p>Logo 19</p>
-            </div>
-            <div class="logo">
-              <p>Logo 20</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="logo">
-              <p>Logo 21</p>
-            </div>
-            <div class="logo">
-              <p>Logo 22</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="logo">
-              <p>Logo 23</p>
-            </div>
-            <div class="logo">
-              <p>Logo 24</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="logo">
-              <p>Logo 25</p>
-            </div>
-            <div class="logo">
-              <p>Logo 26</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="logo">
-              <p>Logo 27</p>
-            </div>
-            <div class="logo">
-              <p>Logo 28</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="logo">
-              <p>Logo 29</p>
-            </div>
-            <div class="logo">
-              <p>Logo 30</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="logo">
-              <p>Logo 31</p>
-            </div>
-            <div class="logo">
-              <p>Logo 32</p>
-            </div>
-          </div>
-        </section>
-
-        <section class="trigger-footer">
-          <img src="./assets/footer.jpg" alt="" />
-        </section>
+        <div className="item">
+          <p>w</p>
+        </div>
+        <div className="item">
+          <p>m</p>
+        </div>
       </div>
-    </main>
+
+      <section className="hero">
+        <img src="/assets/hero.jpg" alt="" />
+      </section>
+
+      <section className="clients">
+        {clients.map((client, i) => (
+          <div ref={(el) => (rows.current[i] = el)} key={i} className="row">
+            <div className="logo">
+              <p>{client.client}</p>
+            </div>
+            <div className="logo">
+              <p>{client.logo}</p>
+            </div>
+          </div>
+        ))}
+      </section>
+
+      <section ref={footerTrigger} className="trigger-footer">
+        <img src="/assets/footer.jpg" alt="" />
+      </section>
+    </div>
   );
 }
